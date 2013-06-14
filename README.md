c47cf1bc62c8c9e5840bc70a2cfa7144486f5f30
Aaron O'Mullan <aaron.omullan@friendco.de>
2013-06-14 16:51:35 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Doks Server
+
+A server to expose docsets over http.
+
+Providing a nice api to query a group of docsets.
---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    tasks: require('./lib/tasks/'),
+
+    server: require('./lib/server'),
+    docset: require('./lib/docset'),
+    docsetgroup: require('./lib/docsetgroup'),
+    docsetindex: require('./lib/docsetindex')
+};
